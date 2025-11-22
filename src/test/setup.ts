4c65036813d3cb1ed99ac68d3720f5140f5ccb07
kimsh2948 <shkim2948@gmail.com>
2025-11-22 17:13:12 +0900

    Revert "test: 테스트 코드 적용"
    This reverts commit 147758a216449a42c3b27eaa68a0d6a40bcb9adc.

--- src/test/setup.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom'
-import { cleanup } from '@testing-library/react'
-import { afterEach } from 'vitest'
-
-// 각 테스트 후 cleanup
-afterEach(() => {
-  cleanup()
-})
-
--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    css: true,
-  },
 })
